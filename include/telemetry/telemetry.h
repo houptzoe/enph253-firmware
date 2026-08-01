11e4f76ee3106accf7f184c900a6764a1dee8380
Hsin Jung Lin <hlin3366@gmail.com>
2026-08-01 12:28:24 -0700

    changes

--- include/telemetry/telemetry.h
+++ include/telemetry/telemetry.h
@@ -11,7 +11,7 @@ struct TelemetrySnapshot {
 };
 
 struct DriveSettings {
-  bool running = false;  // motors off until Start is pressed
+  bool running = true;  // motors off until Start is pressed
   float leftBaseSpeed = 80.0f;
   float rightBaseSpeed = 80.0f;
   float maxSpeed = 150.0f;
